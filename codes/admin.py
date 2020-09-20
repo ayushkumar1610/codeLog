dd317f4897013c503a65ca8a255d69df69bc1ab2
Ayush <ayushkumar1610@outlook.com>
2020-09-20 15:01:41 +0530

    rich text editor added to admin panel

--- codes/admin.py
+++ codes/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Code
 # Register your models here.
 
-admin.site.register(Code)
+# admin.site.register(Code)
+
+@admin.register(Code)
+class CodeAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('tiny.js',)
